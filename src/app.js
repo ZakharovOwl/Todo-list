4677eee3c5019b884c68647550e141193fa78dc1
Aleksander <Zakharov.owl@gmail.com>
2020-09-09 22:47:53 +0300

    feat: add app js

---
+++ src/app.js
@@ -0,0 +1,50 @@
+//Selectors 
+const todoInput = document.querySelector('.todo-input')
+const todoButton = document.querySelector('.todo-button')
+const todoList = document.querySelector('.todo-list')
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo)
+
+
+//Functions
+
+function addTodo(event){
+    //Prevent form from submitting 
+    event.preventDefault();
+    //create todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //Create LI
+
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    //CHECK MARK BUTTON
+
+
+        /*Node.appendChild() добавляет узел в конец списка дочерних элементов указанного родительского узла. 
+        Если данный дочерний элемент является ссылкой на существующий узел в документе, то функция 
+        appendChild() перемещает его из текущей позиции в новую позицию (нет необходимости удалять узел из 
+        родительского узла перед добавлением его к какому-либо другому узлу).
+    
+        Создаем новый элемент параграфа, и вставляем в конец document body
+        var p = document.createElement("p");
+        document.body.appendChild(p);*/
+
+    const completedButton = document.createElement('button')
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+
+    // CHECK trash BUTTON
+    const trashButton = document.createElement('button')
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('complete-btn');
+    todoDiv.appendChild(trashButton);
+    
+    //APPEND TI LIST
+    todoList.appendChild(todoDiv)
+}
