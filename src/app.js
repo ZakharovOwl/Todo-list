8a7414957be2ac6a83d803fb36b718188d1dac35
Aleksander <Zakharov.owl@gmail.com>
2020-09-09 23:02:07 +0300

    feat: add styles from create todo

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ function addTodo(event){
     // CHECK trash BUTTON
     const trashButton = document.createElement('button')
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-    trashButton.classList.add('complete-btn');
+    trashButton.classList.add('trash-btn');
     todoDiv.appendChild(trashButton);
     
     //APPEND TI LIST
