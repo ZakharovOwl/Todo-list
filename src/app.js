771ceda2710760414c99c5f92dd75ae34831b47a
Aleksander <Zakharov.owl@gmail.com>
2020-09-10 21:35:49 +0300

    add deleteChech func

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector('.todo-list')
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo)
+todoList.addEventListener('click', deleteCheck)
 
 
 //Functions
@@ -50,4 +51,20 @@ function addTodo(event){
 
     //Clear value
     todoInput.value =''
+}
+
+function deleteCheck(e){
+    const item = e.target;
+    if (item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        todo.remove()
+    }
+
+
+    //CHECK MARC
+    if(item.classList[0] === 'complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed')
+    }
+
 }
